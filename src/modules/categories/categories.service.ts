d8cd68c8426349ef7c318077fdf9b93e3e04f9bc
Pchenrique <pchenrique1998@hotmail.com>
2023-11-02 22:49:16 -0300

    feat: list all categories by authenticate user

---
+++ src/modules/categories/categories.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { CategoriesRepository } from 'src/shared/database/repositories/categories.repositories';
+
+@Injectable()
+export class CategoriesService {
+  constructor(private readonly categoriesRepository: CategoriesRepository) {}
+
+  findAllByUserId(userId: string) {
+    return this.categoriesRepository.findMany({ where: { userId } });
+  }
+}
--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UsersRepository } from './repositories/users.repositories';
+import { CategoriesRepository } from './repositories/categories.repositories';
 
 @Global()
 @Module({
-  providers: [PrismaService, UsersRepository],
-  exports: [UsersRepository],
+  providers: [PrismaService, UsersRepository, CategoriesRepository],
+  exports: [UsersRepository, CategoriesRepository],
 })
 export class DatabaseModule {}
